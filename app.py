364808748e8d71fec1fc80ee5d5ba0e684ab8ad8
nikhilyadav09 <nikhilyadav1921@gmail.com>
2024-03-09 21:22:25 +0530

    done

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from authlib.integrations.flask_client import OAuth
 
 # Function to connect to PostgreSQL database
 conn = psycopg2.connect(
-    host="localhost",
+    host="news-analyser.onrender.com",
     database="dhp2024",
     user='postgres',
     password='Nikhil@930',
